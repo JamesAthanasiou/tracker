412b2c58b4906e3ea972476febd330e88ec918f3
JamesAthanasiou <james.athanasiou@live.com>
2024-09-30 09:22:27 -0400

    Add in memory test to app. Add pg steps to docker compose.

---
+++ src/database/in_memory_db.ts
@@ -0,0 +1,3 @@
+let db_in_mem: {} = {};
+
+module.exports = db_in_mem;
--- src/routes/test_routes.ts
+++ src/routes/test_routes.ts
@@ -1,12 +1,19 @@
 import { Request, Response, Router } from 'express';
 
+const dbInMem: any = require('../database/in_memory_db');
 const express = require('express');
 const router: Router = express.Router();
-// JTODO this isn't typed, figure out how to use types with TS
+
 const numberGenService: () => number = require('../service/number_gen_service');
 
-router.get("/", (req: Request, res: Response) => {
-  return res.json({ number: numberGenService() });
+router.post("/new-number", (req: Request, res: Response) => {
+  dbInMem['number'] = numberGenService();
+  return res.json({ number: dbInMem['number'] });
+});
+
+router.get("/last-number", (req: Request, res: Response) => {
+  const result = dbInMem['number'] == null ? 'No number set' : dbInMem['number'];
+  return res.json({ number: result });
 });
 
 module.exports = router;
