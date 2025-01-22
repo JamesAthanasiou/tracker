aab806f658e1e63c324f92e393c2938b21e45185
JamesAthanasiou <james.athanasiou@live.com>
2025-01-21 22:43:04 -0500

    Add show person route

--- src/routes/person_routes.ts
+++ src/routes/person_routes.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, Router } from 'express';
 import {
     createPerson,
     findPeople,
+    findPersonById,
 } from '../database/repositories/person_repository';
 import { NewPerson } from '../database/types';
 
@@ -15,6 +16,17 @@ personRouter.get('/routes', (req: Request, res: Response) => {
 
 personRouter.get('/show-all', showAll);
 personRouter.post('/create', create);
+personRouter.get('/:person_id', show);
+
+async function show(req: Request, res: Response) {
+    const person_id = req.params?.person_id;
+
+    if (!person_id) {
+        throw Error('No ID provided');
+    }
+
+    return res.json(await findPersonById(parseInt(person_id)));
+}
 
 // TODO this requires a db to be running. Add a working db to docker compose so we can run it on the ec2 instance.
 async function create(req: Request, res: Response) {
@@ -25,20 +37,11 @@ async function create(req: Request, res: Response) {
 
     let personData: NewPerson | null = null;
 
-    if (!gender && !first_name) {
-        // Default for now. TODO, change. Definitely not the actual behaviour we want.
-        personData = {
-            gender: 'man',
-            first_name: 'John',
-            last_name: 'Doe',
-        };
-    } else {
-        personData = {
-            gender: gender,
-            first_name: first_name,
-            last_name: last_name,
-        };
-    }
+    personData = {
+        gender: gender,
+        first_name: first_name,
+        last_name: last_name,
+    };
 
     const person = await createPerson(personData);
     return res.json(person);
