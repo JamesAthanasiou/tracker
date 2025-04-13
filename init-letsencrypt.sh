ed32dc4cb9775900a3e816562ff8bf6da25b244f
JamesAthanasiou <james.athanasiou@live.com>
2025-04-13 09:40:17 -0400

    Updates for nginx

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(jamesisonline.com www.jamesisonline.com)
+domains=(jamesisonline.com www.jamesisonline.com api.jamesisonline.com)
 rsa_key_size=4096
 data_path="./certbot"
 email="" # Adding a valid address is strongly recommended
